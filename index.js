465b8a47e325b2985497f2df323f10c1afef9f18
Omaroookolosio94 <omarookolosio94@gmail.com>
2020-04-17 01:11:54 +0100

    Updates

--- index.js
+++ index.js
@@ -31,3 +31,18 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+//SVG Loader
+var $loader = document.querySelector('.loader');
+
+window.onload = function () {
+  $loader.classList.remove('loader--active');
+};
+
+// document.querySelector('.btn').addEventListener('click', function () {
+//   $loader.classList.add('loader--active');
+
+//   window.setTimeout(function () {
+//     $loader.classList.remove('loader--active');
+//   }, 5000);
+// });
